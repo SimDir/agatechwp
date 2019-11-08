db3e2b40b9b07a04f8932aaee335eec33a111bcc
Vlad <wizardhype1998@gmail.com>
2019-11-08 12:50:34 +0400

    добавил тему

---
+++ wp-content/themes/agatech.local/js/script.js
@@ -0,0 +1,53 @@
+let first = document.querySelector('#header__menu_link_first');
+first.onmouseover = showBorderFirst;
+first.onmouseout = hideBorderFirst;
+function showBorderFirst() {
+   document.querySelector('#header__menu_link_first').style.borderBottom ="3px solid #fff";
+}
+function hideBorderFirst() {
+   document.querySelector('#header__menu_link_first').style.borderBottom ="none";
+}
+
+
+let two = document.querySelector('#header__menu_link_two');
+two.onmouseover = showBorderTwo;
+two.onmouseout = hideBorderTwo;
+function showBorderTwo() {
+   document.querySelector('#header__menu_link_two').style.borderBottom ="3px solid #fff";
+}
+function hideBorderTwo() {
+   document.querySelector('#header__menu_link_two').style.borderBottom ="none";
+}
+
+
+let three = document.querySelector('#header__menu_link_three');
+three.onmouseover = showBorderThree;
+three.onmouseout = hideBorderThree;
+function showBorderThree() {
+   document.querySelector('#header__menu_link_three').style.borderBottom ="3px solid #fff";
+}
+function hideBorderThree() {
+   document.querySelector('#header__menu_link_three').style.borderBottom ="none";
+}
+
+
+let four = document.querySelector('#header__menu_link_four');
+four.onmouseover = showBorderFour;
+four.onmouseout = hideBorderFour;
+function showBorderFour() {
+   document.querySelector('#header__menu_link_four').style.borderBottom ="3px solid #fff";
+}
+function hideBorderFour() {
+   document.querySelector('#header__menu_link_four').style.borderBottom ="none";
+}
+
+
+let five = document.querySelector('#header__menu_link_five');
+five.onmouseover = showBorderFive;
+five.onmouseout = hideBorderFive;
+function showBorderFive() {
+   document.querySelector('#header__menu_link_five').style.borderBottom ="3px solid #fff";
+}
+function hideBorderFive() {
+   document.querySelector('#header__menu_link_five').style.borderBottom ="none";
+}
