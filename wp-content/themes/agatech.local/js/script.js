cc4572ef9ef3cf4c4ed22c3e6ac7e8434b1ab25b
Vlad <wizardhype1998@gmail.com>
2019-11-11 18:52:33 +0400

    верстаю слайдер

--- wp-content/themes/agatech.local/js/script.js
+++ wp-content/themes/agatech.local/js/script.js
@@ -1,57 +1,83 @@
-let first = document.querySelector('#header__menu_link_first');
+let first = document.querySelector('.header__menu_link_first');
 first.onmouseover = showBorderFirst;
 first.onmouseout = hideBorderFirst;
 function showBorderFirst() {
-   document.querySelector('#header__menu_link_first').style.borderBottom ="3px solid #fff";
+   first.style.borderBottom ="3px solid #fff";
 }
 function hideBorderFirst() {
-   document.querySelector('#header__menu_link_first').style.borderBottom ="none";
+   first.style.borderBottom ="none";
 }
 
 
-let two = document.querySelector('#header__menu_link_two');
+let two = document.querySelector('.header__menu_link_two');
 two.onmouseover = showBorderTwo;
 two.onmouseout = hideBorderTwo;
 function showBorderTwo() {
-   document.querySelector('#header__menu_link_two').style.borderBottom ="3px solid #fff";
+   two.style.borderBottom ="3px solid #fff";
 }
 function hideBorderTwo() {
-   document.querySelector('#header__menu_link_two').style.borderBottom ="none";
+   two.style.borderBottom ="none";
 }
 
 
-let three = document.querySelector('#header__menu_link_three');
+let three = document.querySelector('.header__menu_link_three');
 three.onmouseover = showBorderThree;
 three.onmouseout = hideBorderThree;
 function showBorderThree() {
-   document.querySelector('#header__menu_link_three').style.borderBottom ="3px solid #fff";
+   three.style.borderBottom ="3px solid #fff";
 }
 function hideBorderThree() {
-   document.querySelector('#header__menu_link_three').style.borderBottom ="none";
+   three.style.borderBottom ="none";
 }
 
 
-let four = document.querySelector('#header__menu_link_four');
+let four = document.querySelector('.header__menu_link_four');
 four.onmouseover = showBorderFour;
 four.onmouseout = hideBorderFour;
 function showBorderFour() {
-   document.querySelector('#header__menu_link_four').style.borderBottom ="3px solid #fff";
+   four.style.borderBottom ="3px solid #fff";
 }
 function hideBorderFour() {
-   document.querySelector('#header__menu_link_four').style.borderBottom ="none";
+   four.style.borderBottom ="none";
 }
 
 
-let five = document.querySelector('#header__menu_link_five');
+let five = document.querySelector('.header__menu_link_five');
 five.onmouseover = showBorderFive;
 five.onmouseout = hideBorderFive;
 function showBorderFive() {
-   document.querySelector('#header__menu_link_five').style.borderBottom ="3px solid #fff";
+   five.style.borderBottom ="3px solid #fff";
 }
 function hideBorderFive() {
-   document.querySelector('#header__menu_link_five').style.borderBottom ="none";
+   five.style.borderBottom ="none";
 }
 
+
+function checkurl() {
+   let location = window.location.href;
+   if(location == "http://agatech.local/include/DigitalAgency.html") {
+      showBorderFirst();
+      first.onmouseout = showBorderFirst;
+   }
+   else if(location == "http://agatech.local/include/NewCenter.html") {
+      showBorderTwo();
+      two.onmouseout = showBorderTwo;
+   }
+   else if(location == "http://agatech.local/include/Colutions.html") {
+      showBorderThree();
+      three.onmouseout = showBorderThree;
+   }
+  else if(location == "http://agatech.local/include/Products.html") {
+   showBorderFour();
+      four.onmouseout = showBorderFour;
+   }
+   else if(location == "http://agatech.local/include/BusinessCooperation.html") {
+      showBorderFive();
+      five.onmouseout = showBorderFive;
+   }
+   console.log(location);
+}
+checkurl();
   
 
 
